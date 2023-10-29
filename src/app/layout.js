49503f6cb81db90b66c1a3e68edb13c120822bf9
magzhan <magzhan@1fit.app>
2023-10-30 01:49:26 +0600

    refactor: fix styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} bg-white relative`}>
         {/* <div className='absolute w-[50%] z-0 h-[50%] rounded-full bg-[#FE17BD] right-0 top-[100vh] blur-[100%]' /> */}
         <div className='absolute w-[40%] z-0 h-[35%] rounded-full bg-[#C87800] left-[20%] top-0 blur-[1200px]' />
-        <div className='relative z-20]'>{children}</div>
+        <div className='relative z-20'>{children}</div>
         <div className='relative z-0'>
           <Footer />
         </div>
