f00a317c546129035150fcf733182e90a89d8c39
magzhan <magzhan@1fit.app>
2024-02-08 20:00:07 +0600

    minimal updates

--- src/components/main/Partners.jsx
+++ src/components/main/Partners.jsx
@@ -2,16 +2,25 @@ import Image from 'next/image';
 
 export const Partners = () => {
   return (
-    <div className='max-w-[1440px] px-4 md:px-10 mt-24 md:mt-40 mx-auto pb-[72px] md:pb-[200px] flex md:items-center gap-10 justify-between flex-col md:flex-row'>
-      <p className='font-black text-3xl md:text-4xl'>Наши партнеры</p>
-      <Image width={176} height={64} src='/partners/tan.png' alt='tan' />
-      <Image
-        width={176}
-        height={64}
-        src='/partners/opengate.png'
-        alt='opengate'
-      />
-      <Image width={176} height={64} src='/partners/forbes.svg' alt='forbes' />
+    <div className='max-w-[1440px] mb-24 mt-24 md:mt-40 mx-auto px-4 md:px-10'>
+      <p className='font-black mb-8 text-3xl md:text-6xl russo-one'>
+        Наши партнеры
+      </p>
+      <div className='flex md:items-center gap-10 justify-between flex-col md:flex-row md:justify-start md:gap-24'>
+        <Image width={224} height={96} src='/partners/tan.png' alt='tan' />
+        <Image
+          width={224}
+          height={96}
+          src='/partners/opengate.png'
+          alt='opengate'
+        />
+        <Image
+          width={224}
+          height={96}
+          src='/partners/forbes.svg'
+          alt='forbes'
+        />
+      </div>
     </div>
   );
 };
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -23,6 +23,16 @@ const links = [
 export const Header = ({ color = 'black' }) => {
   const [nav, setNav] = useState(false);
   const router = useRouter();
+  const [isDropdownVisible, setDropdownVisible] = useState(null);
+  const [language, setLanguage] = useState('ru');
+
+  const handleMouseEnter = (idx) => {
+    setDropdownVisible(idx);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(null);
+  };
 
   const handleNavigate = () => router.push('form');
 
@@ -47,23 +57,132 @@ export const Header = ({ color = 'black' }) => {
           className='ml-4 pl-4 font-bold border-l-2 hidden md:block'
           style={{ color: color, borderLeftColor: color }}
         >
-          ИЮЛЬ, 29-31, 2024
+          ИЮЛЬ, 22-28, 2024
         </p>
       </Link>
       <nav className='hidden lg:flex gap-8'>
-        {links.map((link) => (
-          <Link
-            key={link.name}
-            href={link.href}
-            className='hover:underline font-bold'
-            style={{ color: color }}
-          >
-            {link.name}
-          </Link>
-        ))}
+        <p
+          className='hover:underline font-bold cursor-pointer relative'
+          style={{ color: color }}
+          onMouseEnter={() => handleMouseEnter(1)}
+          onMouseLeave={handleMouseLeave}
+        >
+          О фестивале
+          {isDropdownVisible === 1 && (
+            <div className='hidden lg:flex absolute top-[24px] left-[-8px] pt-[10px]'>
+              <div className='flex flex-col gap-2 w-[220px] p-4 rounded-lg bg-white'>
+                <Link
+                  href='/history'
+                  className='hover:underline font-bold'
+                  style={{ color: color }}
+                >
+                  Архивы
+                </Link>
+                <Link
+                  href='/history'
+                  className='hover:underline font-bold'
+                  style={{ color: color }}
+                >
+                  2023 (Возраждение)
+                </Link>
+              </div>
+            </div>
+          )}
+        </p>
+        <Link
+          href='/news'
+          className='hover:underline font-bold'
+          style={{ color: color }}
+        >
+          Новости
+        </Link>
+        <Link
+          href='/contacts'
+          className='hover:underline font-bold'
+          style={{ color: color }}
+        >
+          Контакты
+        </Link>
+        <p
+          className='hover:underline font-bold cursor-pointer relative'
+          style={{ color: color }}
+          onMouseEnter={() => handleMouseEnter(4)}
+          onMouseLeave={handleMouseLeave}
+        >
+          2024
+          {isDropdownVisible === 4 && (
+            <div className='hidden lg:flex absolute top-[24px] left-[-16px] pt-[10px]'>
+              <div className='flex flex-col gap-2 w-[240px] p-4 rounded-lg bg-white'>
+                <Link
+                  href='/'
+                  className='hover:underline font-bold'
+                  style={{ color: color }}
+                >
+                  Правила
+                </Link>
+                <Link
+                  href='/'
+                  className='hover:underline font-bold'
+                  style={{ color: color }}
+                >
+                  Кто может учавствовать
+                </Link>
+                <Link
+                  href='/'
+                  className='hover:underline font-bold'
+                  style={{ color: color }}
+                >
+                  Мероприятие
+                </Link>
+              </div>
+            </div>
+          )}
+        </p>
       </nav>
       <div className='flex items-center relative'>
-        <button
+        <p
+          className='hover:underline font-bold cursor-pointer relative flex items-center justify-between py-2 pr-8'
+          style={{ color: color }}
+          onMouseEnter={() => handleMouseEnter(5)}
+          onMouseLeave={handleMouseLeave}
+        >
+          {language === 'ru' ? 'Ру' : 'En'}{' '}
+          <svg
+            className='w-2.5 h-2.5 ml-2.5'
+            aria-hidden='true'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 10 6'
+          >
+            <path
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='m1 1 4 4 4-4'
+            />
+          </svg>
+          {isDropdownVisible === 5 && (
+            <div className='hidden lg:flex absolute top-[24px] left-[-16px] pt-[10px]'>
+              <div className='flex flex-col gap-2 w-[64px] p-4 rounded-lg bg-white'>
+                <p
+                  onClick={() => setLanguage('ru')}
+                  className='hover:underline font-bold'
+                >
+                  Ру
+                </p>
+                <p
+                  onClick={() => setLanguage('en')}
+                  href='/'
+                  className='hover:underline font-bold'
+                >
+                  En
+                </p>
+              </div>
+            </div>
+          )}
+        </p>
+        {/* <button
           id='dropdownNavbarLink'
           datadropdowntoggle='dropdownNavbar'
           className='flex items-center justify-between py-2 pr-8 font-bold'
@@ -85,7 +204,7 @@ export const Header = ({ color = 'black' }) => {
               d='m1 1 4 4 4-4'
             />
           </svg>
-        </button>
+        </button> */}
         <button
           className='hidden lg:block px-6 py-4 bg-[#FF9E2C] hover:bg-[#FFBF75] rounded-2xl font-bold'
           style={{ color: color }}
@@ -105,16 +224,55 @@ export const Header = ({ color = 'black' }) => {
         </div>
         {nav && (
           <ul className='flex flex-col p-4 justify-center w-[280px] gap-4 absolute top-[40px] left-[-180px] bg-white rounded-lg'>
-            {links.map((link) => (
-              <Link
-                key={link.name}
-                href={link.href}
-                className='hover:underline text-black'
-                onClick={() => setNav(false)}
-              >
-                {link.name}
-              </Link>
-            ))}
+            <Link
+              href='/history'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Архивы
+            </Link>
+            <Link
+              href='/history'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              2023 (Возраждение)
+            </Link>
+            <Link
+              href='/news'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Новости
+            </Link>
+            <Link
+              href='/contacts'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Контакты
+            </Link>
+            <Link
+              href='/'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Правила
+            </Link>
+            <Link
+              href='/'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Кто может учавствовать
+            </Link>
+            <Link
+              href='/'
+              className='hover:underline text-black'
+              style={{ color: color }}
+            >
+              Мероприятие
+            </Link>
             <button
               className='p-4 bg-[#FF9E2C] hover:bg-[#FFBF75] rounded-2xl font-bold text-black'
               onClick={handleNavigate}
