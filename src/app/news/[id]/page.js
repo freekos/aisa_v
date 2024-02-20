16e2d080fafc8e26f3179c00feb42c1c24397480
Gaziz <gazizbolat2@gmail.com>
2024-02-20 18:25:21 +0600

    fix: add nextjs static site features

--- src/app/news/[id]/page.js
+++ src/app/news/[id]/page.js
@@ -1,4 +1,13 @@
 import { Article } from '@/components/news';
+import { news } from '@/consts';
+
+export async function getStaticPaths() {
+  const paths = news.map(post => ({
+      params: { id: post.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+}
 
 export default function ArticlePage() {
   return (
